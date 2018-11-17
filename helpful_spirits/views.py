49b8345cd3df1058b48811153a5159cb88abf791
Maciej Reszke <maciejreszke98@gmail.com>
2018-11-17 17:34:37 +0100

    Working on models

--- helpful_spirits/views.py
+++ helpful_spirits/views.py
@@ -1,5 +1,12 @@
 from helpful_spirits import app
 
+from .models import *
+
+
 @app.route('/')
 def index():
-	return 'Hello, world!'
+	city = City(name='Gdańsk')
+	region = Region(name="Wrzeszcz", city_id=city)
+	location = Location()
+	#user = User()
+	return region.city_id.name
