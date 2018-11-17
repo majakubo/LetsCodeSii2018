a59626683d2a0d89e3489a02d3da3c6efd679d00
Majakubo <m.jakubowski.git@gmail.com>
2018-11-17 19:08:42 +0100

    added skeleton forms to implement

--- helpful_spirits/forms.py
+++ helpful_spirits/forms.py
@@ -1,7 +1,36 @@
 from flask_wtf import Form
-from wtforms.fields import StringField
+from wtforms.fields import StringField, BooleanField, PasswordField, validators
 from flask_wtf.html5 import URLField
 from wtforms.validators import DataRequired, urllib
 
 class SimpleForm(Form):
     name = StringField('description')
+
+
+class Register(Form):
+    username = StringField('Username')
+    password
+    password_repetition
+    birth_date
+    mail
+    tel
+
+
+class Login(Form):
+    username
+    password
+
+
+class SearchCity(Form):
+    city_name
+
+
+class AddPoster(Form):
+    start_date
+    end_date
+    title
+    description
+    location
+    category
+
+
