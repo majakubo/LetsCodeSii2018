9d613d7ac62ab6752a2d1e6bd30a124d610e618c
Majakubo <m.jakubowski.git@gmail.com>
2018-11-17 15:17:35 +0100

    added project skeleton

---
+++ helpful_spirits/views.py
@@ -0,0 +1,5 @@
+from HelpfulSpirits import app
+
+@app.route('/')
+def index():
+	return 'Hello, world!'
---
+++ manage.py
@@ -0,0 +1,9 @@
+# manage.py
+
+from flask_script import Manager
+from .helpful_spirits import app
+
+manager = Manager(app)
+
+if __name__ == "__main__":
+    manager.run()
