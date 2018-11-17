ababf325df64b992e3ba5e851bdfd57b576406de
Majakubo <m.jakubowski.git@gmail.com>
2018-11-17 18:38:35 +0100

    added endpoints to view

--- helpful_spirits/views.py
+++ helpful_spirits/views.py
@@ -2,18 +2,36 @@ from helpful_spirits import app
 from flask import render_template
 from .models import *
 
-
-@app.route('/my')
-def my():
-	city = City(name='Gdansk')
-	region = Region(name="Wrzeszcz", city_id=city)
-
-	location = Location()
-	#user = User()
-	return region.city_id.name
-
-
 @app.route('/')
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
+
+@app.route('/login')
+def login():
+    return "You are in login site"
+
+
+@app.route('/register')
+def register():
+    return "You are in register site"
+
+
+@app.route('/add_poster')
+def add_poster():
+    return "You are in add poster site"
+
+
+@app.route('/posters')
+def posters():
+    return "You are in posters site"
+
+
+@app.route('/posters/<id>')
+def poster(id):
+    return "you are at specific site"
+
+
+@app.route('/my_profile')
+def my_profile():
+    return "You are in my profile site"
