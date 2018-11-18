31ef8a659ac6168c74f66d19072a52e1f2ae82c6
Majakubo <m.jakubowski.git@gmail.com>
2018-11-18 01:51:00 +0100

    added filter

--- helpful_spirits/models.py
+++ helpful_spirits/models.py
@@ -76,6 +76,9 @@ class Specialisation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
+    @staticmethod
+    def get_all():
+        return Specialisation.query.all()
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
