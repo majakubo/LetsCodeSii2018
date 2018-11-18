56032704c66584433bc478ca9a5b4870a8231eb0
Majakubo <m.jakubowski.git@gmail.com>
2018-11-18 17:31:16 +0100

    deleted unimplemented tests

--- test/helpful_test.py
+++
@@ -1,9 +0,0 @@
-from flask import url_for
-from flask_testing import TestCase
-import helpful_spirits
-from helpful_spirits.models import *
-
-class HelpfulSpiritsTestCase(TestCase):
-
-    def create_app(self):
-        return helpful_spirits.create_app('test')
